0dc9ced85c6fead07f6ada62239437332d5a2f49
amargui01 <amargui01@gmail.com>
2025-02-15 21:18:41 -0500

    OOP tutorials

--- oop1.py
+++ oop1.py
@@ -6,12 +6,14 @@ class Employee:
         self.email = first + '.' + last + '@email.com'
         self.pay = pay
 
+    # retun employee full name
     def fullname(self):
         return  f'{self.first} {self.last}'
 
 
 emp_1 = Employee('youss', 'amar', 50000)
 emp_2 = Employee('Joe', 'rama', 60000)
+
 
 print(emp_1.fullname())
 print(emp_2.fullname())
